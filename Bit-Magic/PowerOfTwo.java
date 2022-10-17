50c8315ce40f7b46635b69ff38ef1f3ed57861ef
om-umrania-code <60146744+om-umrania@users.noreply.github.com>
2022-10-17 15:39:59 +0530

    Summary

--- Bit-Magic/PowerOfTwo.java
+++
@@ -1,7 +0,0 @@
-/*https://leetcode.com/problems/power-of-two/*/
-
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return (n > 0) && (n & (n - 1)) == 0;
-    }
-}
--- Miscellaneous/SmallestEvenMultiple.java
+++
@@ -1,8 +0,0 @@
-/*https://leetcode.com/problems/smallest-even-multiple/*/
-
-class Solution {
-    public int smallestEvenMultiple(int n) {
-        if (n%2 == 0) return n;
-        else return n*2;
-    }
-}
